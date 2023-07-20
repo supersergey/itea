69cbf1e9cef04491a7158f7d1bca8647a20e8ca3
AndriiHliuza <gl093770@gmail.com>
2023-07-20 21:25:32 +0300

    Removed implementation of PostRepository in PostRepositoryInMemoryImpl

--- src/main/java/com/example/demo/repository/PostRepositoryInMemoryImpl.java
+++ src/main/java/com/example/demo/repository/PostRepositoryInMemoryImpl.java
@@ -9,41 +9,36 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Repository
-public class PostRepositoryInMemoryImpl implements PostRepository {
+public class PostRepositoryInMemoryImpl {
     private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
 
     private int counter;
     private int nextId() {
         return counter++;
     }
-    @Override
+
     public int save(Post post) {
         int id = nextId();
         posts.put(id, post);
         return id;
     }
 
-    @Override
     public List<Post> getByUserId(int userId) {
         return Collections.emptyList();
     }
 
-    @Override
     public void delete(int postId) {
         posts.remove(postId);
     }
 
-    @Override
     public int countByUserId(int userId) {
         return getByUserId(userId).size();
     }
 
-    @Override
     public boolean existsById(int postId) {
         return posts.containsKey(postId);
     }
 
-    @Override
     public Post getById(int postId) {
         return posts.get(postId);
     }
