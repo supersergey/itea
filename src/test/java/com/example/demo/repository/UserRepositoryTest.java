b853e2b3b31e007e00d7975b100a66e51c398e01
sergiytolokunsky <sergey.tolokunsky@gmail.com>
2023-07-20 11:33:19 +0200

    Lection 6: homework

--- src/test/java/com/example/demo/repository/UserRepositoryTest.java
+++ src/test/java/com/example/demo/repository/UserRepositoryTest.java
@@ -9,7 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-class UserRepositoryImplTest {
+class UserRepositoryTest {
 
     @Autowired
     @Qualifier("springDataUserRepository")
