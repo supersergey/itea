227fbb3f231171481a45f3f94f33a0c5ddae0828
sergiytolokunsky <sergey.tolokunsky@gmail.com>
2023-07-31 19:16:24 +0200

    Lection 10: in-lection coding

--- src/main/java/com/example/demo/webclient/OpenWeatherFeignClient.java
+++ src/main/java/com/example/demo/webclient/OpenWeatherFeignClient.java
@@ -1,14 +1,12 @@
 package com.example.demo.webclient;
 
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import feign.Param;
+import feign.RequestLine;
 
-@FeignClient(value = "openWeather", url = "https://api.openweathermap.org/data/2.5/forecast")
 public interface OpenWeatherFeignClient {
-    @GetMapping
-    Forecast getForecast(@RequestParam(value = "lat") String latitude,
-                         @RequestParam(value = "lon") String longitude,
-                         @RequestParam String units,
-                         @RequestParam String apiKey);
+    @RequestLine("GET")
+    Forecast getForecast(@Param(value = "lat") String latitude,
+                         @Param(value = "lon") String longitude,
+                         @Param String units,
+                         @Param String apiKey);
 }
--- src/main/java/com/example/demo/webclient/WebClientConfig.java
+++ src/main/java/com/example/demo/webclient/WebClientConfig.java
@@ -1,17 +1,19 @@
-//package com.example.demo.webclient;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import feign.Feign;
-//import feign.gson.GsonDecoder;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class WebClientConfig {
-//    @Bean
-//    public OpenWeatherFeignClient getFeignClient(ObjectMapper objectMapper) {
-//        return Feign.builder()
-//                .decoder(new GsonDecoder())
-//                .target(OpenWeatherFeignClient.class, "https://api.openweathermap.org/data/2.5/forecast");
-//    }
-//}
+package com.example.demo.webclient;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import feign.Feign;
+import feign.jackson.JacksonDecoder;
+import feign.jackson.JacksonEncoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebClientConfig {
+    @Bean
+    public OpenWeatherFeignClient getFeignClient(ObjectMapper objectMapper) {
+        return Feign.builder()
+                .decoder(new JacksonDecoder(objectMapper))
+                .encoder(new JacksonEncoder(objectMapper))
+                .target(OpenWeatherFeignClient.class, "https://api.openweathermap.org/data/2.5/forecast");
+    }
+}
